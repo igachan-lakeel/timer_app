39c70d4bbb54edd2947cb906e7b9e7d58054fd40
igachan-lakeel <igaue-re@lakeel.com>
2026-03-03 17:41:57 +0900

    add カリキュラムテスト一回目実行

---
+++ src/Parent.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import Children from "./components/Children.vue";
+
+const title = "このタイトルは親コンポーネントから渡されています";
+const description = "この説明文は親コンポーネントから渡されています";
+const isVisible = ref(false);
+
+const toggle = () => {
+  isVisible.value = !isVisible.value;
+};
+</script>
+
+<template>
+  <div id="app">
+    <div>
+      <button @click="toggle">表示にする</button>
+      <Children
+        :title="title"
+        :description="description"
+        :isVisible="isVisible"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/Children.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  title: String,
+  description: String,
+  isVisible: Boolean,
+});
+</script>
+
+<template>
+  <div v-if="isVisible">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
